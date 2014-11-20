ef2554a8ddee7a1c5bf335460a3478e6cc219710
Willie Rogers <wjrogers@mail.nih.gov>
2014-11-20 13:43:35 -0500

    Added static method ChemDNER.instantiateDocument
    New method for instantiating BioCreative format ChemDNER documents.
    (one document per line).

--- src/main/java/gov/nih/nlm/nls/metamap/document/ChemDNER.java
+++ src/main/java/gov/nih/nlm/nls/metamap/document/ChemDNER.java
@@ -35,4 +35,26 @@ public class ChemDNER extends PubMedDocumentImpl implements PubMedDocument  {
 
     return new ChemDNER(docFields[0], bodyFields[0], bodyFields[1]);
   }
+
+  /**
+   * Instantiate ChemDNER document instance BioCreative
+   * version of CHEMDNER document.
+   * <p>
+   * Each document consists of docid followed by tab followed by the title
+   * which is followed by a tab and the abstract.
+   *
+   * <pre>
+   * id\ttitle\tabstract
+   * </pre>
+   *
+   * @param docText string containing tab delimited version of
+   * CHEMDNER document.
+   * @return ChemDNER document instance
+   */
+  public static ChemDNER instantiateDocument(String docText) 
+  {
+    String[] docFields = docText.split("\\t");
+    return new ChemDNER(docFields[0], docFields[1], docFields[2]);
+  }
+
 }
