60d7218f9f1289985020ac9dfdd0a20c52ac31a6
Willie Rogers <wjrogers@mail.nih.gov>
2014-12-02 13:04:01 -0500

    Wrapper methods for Wendy Chapman's Context library.

---
+++ src/main/java/gov/nih/nlm/nls/metamap/lite/context/ContextWrapper.java
@@ -0,0 +1,70 @@
+
+//
+package gov.nih.nlm.nls.metamap.lite.context;
+
+import java.util.List;
+import java.util.ArrayList;
+import gov.nih.nlm.nls.metamap.lite.types.Entity;
+import gov.nih.nlm.nls.metamap.lite.types.BioCEntity;
+import gov.nih.nlm.nls.types.Sentence;
+
+import context.implementation.ConText;
+
+import bioc.BioCSentence;
+import bioc.BioCAnnotation;
+
+/**
+ *
+ * method applyContext returns result 
+ */
+
+public class ContextWrapper {
+  static ConText contextInstance = new ConText();
+
+  public static List<List<String>> applyContext(List<String> conceptList, List<String> sentenceList) 
+    throws Exception {
+    List<List<String>> resultlist = new ArrayList<List<String>>();
+    for (String concept: conceptList) {
+      for (String sentence: sentenceList) {
+	resultlist.add(contextInstance.applyContext(concept, sentence));
+      }
+    }
+    return resultlist;
+  }
+
+  public static List<List<String>> applyContextUsingEntities(List<Entity> entityList, 
+							     List<Sentence> sentenceList) 
+    throws Exception {
+    List<List<String>> resultlist = new ArrayList<List<String>>();
+    for (Entity entity: entityList) {
+      for (String matchedWord: entity.getMatchedWordSet()) {
+	for (Sentence sentence: sentenceList) {
+	  resultlist.add(contextInstance.applyContext(matchedWord, sentence.getText()));
+	}
+      }
+    }
+    return resultlist;
+  }
+
+  /** given annotated sentence with entities, determine hedging relations using context.*/
+  public static BioCSentence applyContext(BioCSentence sentence) throws Exception {
+    for (BioCAnnotation annotation: sentence.getAnnotations()) {
+      if (annotation instanceof BioCEntity) {
+	List<String> result = 
+	  contextInstance.applyContext(((BioCEntity)annotation).getEntity().getMatchedText(), 
+				       sentence.getText());
+	if (result != null) {
+	  /** add negation information to existing annotation */
+	  annotation.putInfon("concept",     result.get(0));
+	  annotation.putInfon("sentence",    result.get(1));
+	  annotation.putInfon("negstatus",   result.get(2));
+	  annotation.putInfon("temporality", result.get(3));
+	  annotation.putInfon("subject",     result.get(4));
+	}
+      }
+    }
+    return sentence;
+  }
+
+}
+ 
