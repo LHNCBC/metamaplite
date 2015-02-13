d4aad4464536729ae346c0493fc5513ee47fac33
Willie Rogers <wjrogers@mail.nih.gov>
2015-02-13 11:15:28 -0500

    Reader/Loader for ChemDNER MetaMap format documents.

---
+++ src/main/java/gov/nih/nlm/nls/metamap/document/ChemDNERSLDI.java
@@ -0,0 +1,130 @@
+//
+package gov.nih.nlm.nls.metamap.document;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.List;
+import java.util.ArrayList;
+
+import bioc.BioCDocument;
+import bioc.BioCPassage;
+import gov.nih.nlm.nls.types.Document;
+
+public class ChemDNERSLDI implements BioCDocumentLoader {
+  /**
+   * Instantiate PubMedDocumentImpl document instance reading single-line
+   * delimited version of CHEMDNER document.
+   * <p>
+   * Each sldi document consists of docid followed by separator (|)
+   * followed by title which is followed by a tab and abstract.
+   *
+   * <pre>
+   * id|title\tabstract
+   * </pre>
+   *
+   * @param docText string containing single-line delimited version of
+   * CHEMDNER document.
+   * @return PubMedDocumentImpl document instance
+   */
+  public static PubMedDocument instantiateSLDIDocument(String docText) 
+  {
+    String[] docFields = docText.split("\\|");
+    String docBody = docFields[1];
+    String[] bodyFields = docBody.split("\t");
+
+    return new PubMedDocumentImpl(docFields[0], bodyFields[0], bodyFields[1]);
+  }
+
+
+  /**
+   * Instantiate BioCDocument document instance reading single-line
+   * delimited version of CHEMDNER document.
+   * <p>
+   * Each sldi document consists of docid followed by separator (|)
+   * followed by title which is followed by a tab and abstract.
+   *
+   * <pre>
+   * id|title\tabstract
+   * </pre>
+   *
+   * @param docText string containing single-line delimited version of
+   * CHEMDNER document.
+   * @return BioCDocument document instance
+   */
+  public static BioCDocument instantiateBioCSLDIDocument(String docText) 
+  {
+    String[] docFields = docText.split("\\|");
+    String docBody = docFields[1];
+    String[] bodyFields = docBody.split("\t");
+
+    BioCDocument doc = new BioCDocument();
+    doc.setID(docFields[0]);
+    BioCPassage title = new BioCPassage();
+    title.setText(bodyFields[0]);
+    title.setOffset(0);
+    title.putInfon("title","title");
+    doc.addPassage(title);
+    BioCPassage abstractPassage = new BioCPassage();
+    abstractPassage.setText(bodyFields[1]);
+    abstractPassage.putInfon("abstract","abstract");
+    abstractPassage.setOffset(0);
+    doc.addPassage(abstractPassage);
+    return doc;
+  }
+
+  /**
+   * Load list of PubMedDocumentImpl documents
+   * @param inputFilename input text filename
+   * @return List of strings, one document per line.
+   */
+  public static List<PubMedDocument> loadSLDIFile(String inputFilename)
+    throws FileNotFoundException, IOException
+  {
+    BufferedReader br = new BufferedReader(new FileReader(inputFilename));
+    List<PubMedDocument> documentList = new ArrayList<PubMedDocument>();
+    String line;
+    while ((line = br.readLine()) != null) {
+      documentList.add(ChemDNERSLDI.instantiateSLDIDocument(line));
+    }
+    br.close();
+    return documentList;
+  }
+
+  /**
+   * Load list of BioCDocument documents
+   * @param inputFilename input text filename
+   * @return List of strings, one document per line.
+   */
+  public static List<BioCDocument> bioCLoadSLDIFile(String inputFilename)
+    throws FileNotFoundException, IOException
+  {
+    BufferedReader br = new BufferedReader(new FileReader(inputFilename));
+    List<BioCDocument> documentList = new ArrayList<BioCDocument>();
+    String line;
+    while ((line = br.readLine()) != null) {
+      documentList.add(ChemDNERSLDI.instantiateBioCSLDIDocument(line));
+    }
+    br.close();
+    return documentList;
+  }
+
+
+  public BioCDocument loadFileAsBioCDocument(String filename) 
+    throws FileNotFoundException, IOException
+  {
+    String inputtext = FreeText.loadFile(filename);
+    BioCDocument document = instantiateBioCSLDIDocument(inputtext);
+    return document;
+  }
+  
+ public List<BioCDocument> loadFileAsBioCDocumentList(String filename) 
+    throws FileNotFoundException, IOException
+  {
+    return bioCLoadSLDIFile(filename);
+  }
+    
+}
+
