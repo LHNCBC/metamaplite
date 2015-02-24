0511e9de832dcae8fe6d7564d169c3bf9a1a1e0d
Willie Rogers <wjrogers@mail.nih.gov>
2015-02-24 10:29:23 -0500

    Install Maven Artifacts script for Windows

---
+++ install-maven-artifacts.bat
@@ -0,0 +1,34 @@
+
+REM You could install an artifact on a specific local repository by
+REM setting the localRepositoryPath parameter when installing.
+REM
+REM mvn install:install-file  -Dfile=path-to-your-artifact-jar \
+REM                          -DgroupId=your.groupId \
+REM                          -DartifactId=your-artifactId \
+REM                          -Dversion=version \
+REM                          -Dpackaging=jar \
+REM                          -DlocalRepositoryPath=path-to-specific-local-repo (optional)
+
+mvn install:install-file  -Dfile=lib/biolemmatizer-core-1.2.jar \
+                          -DgroupId=edu.ucdenver.ccp \
+                          -DartifactId=biolemmatizer-core \
+                          -Dversion=1.2 \
+                          -Dpackaging=jar 
+
+mvn install:install-file  -Dfile=lib/bioc_1.0.1.jar \
+                          -DgroupId=bioc \
+                          -DartifactId=bioc \
+                          -Dversion=1.0.1 \
+                          -Dpackaging=jar 
+
+mvn install:install-file  -Dfile=lib/Context.jar \
+                          -DgroupId=context \
+                          -DartifactId=context \
+                          -Dversion=2012 \
+                          -Dpackaging=jar 
+
+mvn install:install-file  -Dfile=lib/nlp-2.4.C.jar \
+                          -DgroupId=gov/nih/nlm/nls \
+                          -DartifactId=nlp \
+                          -Dversion=2.4.C \
+                          -Dpackaging=jar
