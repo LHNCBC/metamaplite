9b1427adfa4a25216d88eba5204f8927cd7057fd
Willie Rogers <wjrogers@mail.nih.gov>
2015-02-24 09:55:32 -0500

    Made install maven artifacts script package specific.
    Made install maven artifacts script package specific using artifacts
    from lib directory.  Moved local install maven artifacts script to
    install-local-maven-artifacts.sh.

--- install-local-maven-artifacts.sh
+++ install-local-maven-artifacts.sh
@@ -9,27 +9,24 @@
 #                          -Dpackaging=jar \
 #                          -DlocalRepositoryPath=path-to-specific-local-repo (optional)
 
-mvn install:install-file  -Dfile=lib/biolemmatizer-core-1.2.jar \
+mvn install:install-file  -Dfile=/usr/local/pub/nlp/BioC/BioC_Java_1.0.1/lib/biolemmatizer-core-1.1-jar-with-dependencies.jar \
                           -DgroupId=edu.ucdenver.ccp \
                           -DartifactId=biolemmatizer-core \
                           -Dversion=1.2 \
                           -Dpackaging=jar 
 
 
-mvn install:install-file  -Dfile=lib/bioc_1.0.1.jar \
+mvn install:install-file  -Dfile=/usr/local/pub/nlp/BioC/BioC_Java_1.0.1/bioc_1.0.1.jar \
                           -DgroupId=bioc \
                           -DartifactId=bioc \
                           -Dversion=1.0.1 \
                           -Dpackaging=jar 
 
-mvn install:install-file  -Dfile=lib/Context.jar \
+
+mvn install:install-file  -Dfile=/usr/local/pub/nlp/JavaConText/Context.jar \
                           -DgroupId=context \
                           -DartifactId=context \
                           -Dversion=2012 \
                           -Dpackaging=jar 
 
-mvn install:install-file  -Dfile=lib/nlp-2.4.C.jar \
-                          -DgroupId=gov/nih/nlm/nls \
-                          -DartifactId=nlp \
-                          -Dversion=2.4.C \
-                          -Dpackaging=jar
+
