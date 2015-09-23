b3a411227d8dcc09da0ac7c93535bec9dbc09e61
Willie Rogers <wjrogers@mail.nih.gov>
2015-09-23 13:43:56 -0400

    Inverted File implementation of Entity tables.

---
+++ src/main/java/gov/nih/nlm/nls/metamap/lite/metamap/MetaMapIvfIndexes.java
@@ -0,0 +1,68 @@
+package gov.nih.nlm.nls.metamap.lite.metamap;
+
+import java.io.InputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+import java.util.Properties;
+
+import irutils.MappedMultiKeyIndex;
+import irutils.MappedMultiKeyIndexLookup;
+
+/**
+ * Tables from MetaMap imported into inverted files.
+ */
+
+public class MetaMapIvfIndexes {
+  /** cui -> source index */
+  public MappedMultiKeyIndexLookup cuiSourceInfoIndex;
+  /** cui -> semantic type index */
+  public MappedMultiKeyIndexLookup cuiSemanticTypeIndex;
+  /** cui -> concept index (actually bi-directional) */
+  public MappedMultiKeyIndexLookup cuiConceptIndex;
+
+  String root =
+    // "/net/lhcdevfiler/vol/cgsb5/ind/II_Group_WorkArea/wjrogers/data/mult-key-index/strict/indices";
+    "data/ivf/strict/indices";
+
+  public MetaMapIvfIndexes() 
+    throws IOException, FileNotFoundException
+  {
+    // indexes
+    this.cuiSourceInfoIndex = 
+      new MappedMultiKeyIndexLookup
+      (new MappedMultiKeyIndex
+       (System.getProperty("metamaplite.ivf.cuisourceinfoindex", root + "/cuisourceinfo")));
+    this.cuiSemanticTypeIndex =
+      new MappedMultiKeyIndexLookup
+      (new MappedMultiKeyIndex
+       (System.getProperty("metamaplite.ivf.cuisemantictypeindex", root + "/cuist")));
+    this.cuiConceptIndex =
+      new MappedMultiKeyIndexLookup
+      (new MappedMultiKeyIndex
+       (System.getProperty("metamaplite.ivf.cuiconceptindex", root + "/cuiconcept")));
+
+  }
+
+  public MetaMapIvfIndexes(Properties properties) 
+    throws IOException, FileNotFoundException
+  {
+    // indexes
+    this.cuiSourceInfoIndex =
+      new MappedMultiKeyIndexLookup
+      (new MappedMultiKeyIndex
+       (properties.getProperty("metamaplite.ivf.cuisourceinfoindex", root + "/cuisourceinfo")));
+    this.cuiSemanticTypeIndex =
+      new MappedMultiKeyIndexLookup
+      (new MappedMultiKeyIndex
+       (properties.getProperty("metamaplite.ivf.cuisemantictypeindex", root + "/cuist")));
+    this.cuiConceptIndex =
+      new MappedMultiKeyIndexLookup
+      (new MappedMultiKeyIndex
+       (System.getProperty("metamaplite.ivf.cuiconceptindex", root + "/cuiconcept")));
+  }
+
+}
