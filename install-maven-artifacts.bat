ab44fe0184e05aeca620cc72cea9541df289cd63
Willie Rogers <wjrogers@mail.nih.gov>
2015-09-29 14:18:06 -0400

    Replaced unix continuation character with MS version.

--- install-maven-artifacts.bat
+++ install-maven-artifacts.bat
@@ -2,39 +2,39 @@
 REM You could install an artifact on a specific local repository by
 REM setting the localRepositoryPath parameter when installing.
 REM
-REM mvn install:install-file  -Dfile=path-to-your-artifact-jar \
-REM                          -DgroupId=your.groupId \
-REM                          -DartifactId=your-artifactId \
-REM                          -Dversion=version \
-REM                          -Dpackaging=jar \
+REM mvn install:install-file  -Dfile=path-to-your-artifact-jar ^
+REM                          -DgroupId=your.groupId ^
+REM                          -DartifactId=your-artifactId ^
+REM                          -Dversion=version ^
+REM                          -Dpackaging=jar ^
 REM                          -DlocalRepositoryPath=path-to-specific-local-repo (optional)
 
-mvn install:install-file  -Dfile=lib/biolemmatizer-core-1.2.jar \
-                          -DgroupId=edu.ucdenver.ccp \
-                          -DartifactId=biolemmatizer-core \
-                          -Dversion=1.2 \
+mvn install:install-file  -Dfile=lib/biolemmatizer-core-1.2.jar ^
+                          -DgroupId=edu.ucdenver.ccp ^
+                          -DartifactId=biolemmatizer-core ^
+                          -Dversion=1.2 ^
                           -Dpackaging=jar 
 
-mvn install:install-file  -Dfile=lib/bioc-1.0.1.jar \
-                          -DgroupId=bioc \
-                          -DartifactId=bioc \
-                          -Dversion=1.0.1 \
+mvn install:install-file  -Dfile=lib/bioc-1.0.1.jar ^
+                          -DgroupId=bioc ^
+                          -DartifactId=bioc ^
+                          -Dversion=1.0.1 ^
                           -Dpackaging=jar 
 
-mvn install:install-file  -Dfile=lib/context-2012.jar \
-                          -DgroupId=context \
-                          -DartifactId=context \
-                          -Dversion=2012 \
+mvn install:install-file  -Dfile=lib/context-2012.jar ^
+                          -DgroupId=context ^
+                          -DartifactId=context ^
+                          -Dversion=2012 ^
                           -Dpackaging=jar 
 
-mvn install:install-file  -Dfile=lib/nlp-2.4.C.jar \
-                          -DgroupId=gov.nih.nlm.nls \
-                          -DartifactId=nlp \
-                          -Dversion=2.4.C \
+mvn install:install-file  -Dfile=lib/nlp-2.4.C.jar ^
+                          -DgroupId=gov.nih.nlm.nls ^
+                          -DartifactId=nlp ^
+                          -Dversion=2.4.C ^
                           -Dpackaging=jar
 
-mvn install:install-file  -Dfile=lib/irutils-2.0-SNAPSHOT.jar \
-                          -DgroupId=irutils \
-                          -DartifactId=irutils \
-                          -Dversion=2.0-SNAPSHOT \
+mvn install:install-file  -Dfile=lib/irutils-2.0-SNAPSHOT.jar ^
+                          -DgroupId=irutils ^
+                          -DartifactId=irutils ^
+                          -Dversion=2.0-SNAPSHOT ^
                           -Dpackaging=jar
