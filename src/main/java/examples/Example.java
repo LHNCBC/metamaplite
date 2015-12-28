b494590a86f45968d86dc47cc3c76558c23c00cd
Willie Rogers <wjrogers@mail.nih.gov>
2015-12-28 15:07:39 -0500

    Two examples of using MetaMapLite from Java.

---
+++ src/main/java/examples/Example.java
@@ -0,0 +1,53 @@
+//
+package examples;
+import bioc.BioCDocument;
+import gov.nih.nlm.nls.metamap.document.FreeText;
+import gov.nih.nlm.nls.metamap.lite.types.Entity;
+import gov.nih.nlm.nls.metamap.lite.types.Ev;
+import gov.nih.nlm.nls.ner.MetaMapLite;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Properties;
+import java.lang.reflect.InvocationTargetException;
+import java.io.IOException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Example of using metaMapLite from Java
+ */
+
+public class Example {
+ /** log4j logger instance */
+  private static final Logger logger = LogManager.getLogger(MetaMapLite.class);
+
+  /**
+   * Main program
+   * @param args - Arguments passed from the command line
+   */
+  public static void main(String[] args)
+    throws Exception, IOException, ClassNotFoundException, InstantiationException,
+	   NoSuchMethodException, IllegalAccessException,
+	   InvocationTargetException
+  {
+    Properties myProperties = new Properties();
+    MetaMapLite.expandModelsDir(myProperties,
+				"/export/home/wjrogers/Projects/metamaplite/data/models");
+    MetaMapLite.expandIndexDir(myProperties,
+			       "/export/home/wjrogers/Projects/metamaplite/data/ivf/strict");
+    myProperties.setProperty("metamaplite.excluded.termsfile",
+			     "/export/home/wjrogers/Projects/metamaplite/data/specialterms.txt");
+    MetaMapLite metaMapLiteInst = new MetaMapLite(myProperties);
+  
+    BioCDocument document = FreeText.instantiateBioCDocument("diabetes");
+    List<BioCDocument> documentList = new ArrayList<BioCDocument>();
+    documentList.add(document);
+    List<Entity> entityList = metaMapLiteInst.processDocumentList(documentList);
+    for (Entity entity: entityList) {
+      for (Ev ev: entity.getEvSet()) {
+	System.out.print(ev.getConceptInfo().getCUI() + "|" + entity.getMatchedText());
+	System.out.println();
+      }
+    }
+  }
+}
---
+++ src/main/java/examples/Example2.java
@@ -0,0 +1,55 @@
+//
+package examples;
+import bioc.BioCDocument;
+import gov.nih.nlm.nls.metamap.document.FreeText;
+import gov.nih.nlm.nls.metamap.lite.types.Entity;
+import gov.nih.nlm.nls.metamap.lite.types.Ev;
+import gov.nih.nlm.nls.ner.MetaMapLite;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Properties;
+import java.lang.reflect.InvocationTargetException;
+import java.io.IOException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Example of using MetaMapLite from Java
+ */
+
+public class Example2 {
+ /** log4j logger instance */
+  private static final Logger logger = LogManager.getLogger(MetaMapLite.class);
+
+  /**
+   * Main program
+   * @param args - Arguments passed from the command line
+   */
+  public static void main(String[] args)
+    throws Exception, IOException, ClassNotFoundException, InstantiationException,
+	   NoSuchMethodException, IllegalAccessException,
+	   InvocationTargetException
+  {
+    Properties myProperties = new Properties();
+    myProperties.setProperty("opennlp.models.directory", 
+			     "/export/home/wjrogers/Projects/metamaplite/data/models");
+    MetaMapLite.expandModelsDir(myProperties);
+    myProperties.setProperty("metamaplite.index.directory",
+			     "/export/home/wjrogers/Projects/metamaplite/data/ivf/strict");
+    myProperties.setProperty("metamaplite.excluded.termsfile",
+			     "/export/home/wjrogers/Projects/metamaplite/data/specialterms.txt");
+    MetaMapLite.expandIndexDir(myProperties);
+    MetaMapLite metaMapLiteInst = new MetaMapLite(myProperties);
+  
+    BioCDocument document = FreeText.instantiateBioCDocument("FDA has strengthened the warning for the type 2 diabetes medicine canagliflozin (Invokana, Invokamet) related to the increased risk of bone fractures, and added new information about decreased bone mineral density. To address these safety concerns, FDA added a new Warning and Precaution and revised the Adverse Reactions section of the Invokana and Invokamet drug labels.");
+    List<BioCDocument> documentList = new ArrayList<BioCDocument>();
+    documentList.add(document);
+    List<Entity> entityList = metaMapLiteInst.processDocumentList(documentList);
+    for (Entity entity: entityList) {
+      for (Ev ev: entity.getEvSet()) {
+	System.out.print(ev.getConceptInfo().getCUI() + "|" + entity.getMatchedText());
+	System.out.println();
+      }
+    }
+  }
+}
