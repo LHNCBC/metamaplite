d6afbb7bcb94bc18de03810f46c6c0af48031655
Willie Rogers <wjrogers@mail.nih.gov>
2016-03-16 16:44:12 -0400

    UMLS Concept Unique Identifier Result Output Formatter

---
+++ src/main/java/gov/nih/nlm/nls/metamap/lite/resultformats/CuiList.java
@@ -0,0 +1,50 @@
+package gov.nih.nlm.nls.metamap.lite.resultformats;
+
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.HashSet;
+import gov.nih.nlm.nls.metamap.lite.types.Entity;
+import gov.nih.nlm.nls.metamap.lite.types.Ev;
+import gov.nih.nlm.nls.metamap.lite.resultformats.ResultFormatter;
+
+/**
+ * CUI List Output 
+ *
+ * Output is UMLS Concept Unique Indentifiers (CUIs), one CUI per line.
+ */
+
+public class CuiList implements ResultFormatter {
+
+  public static String entityToString(Entity entity){
+    StringBuilder sb = new StringBuilder();
+    for (Ev ev: entity.getEvSet()) {
+      sb.append(ev.getConceptInfo().getCUI()).append('\n');
+    }
+    return sb.toString();
+  }
+
+  public static void displayEntityList(PrintWriter pw, List<Entity> entityList) 
+  {
+    Collections.reverse(entityList);
+    Set<String> stringSet = new HashSet<String>();
+    for (Entity entity: entityList) {
+      if (entity.getEvSet().size() > 0) {
+	stringSet.add(entityToString(entity));
+      }
+    }
+    StringBuilder sb = new StringBuilder();
+    for (String resultString: stringSet) {
+      pw.print(resultString);
+    }
+  }
+
+  public void entityListFormatter(PrintWriter writer,
+				  List<Entity> entityList) {
+    displayEntityList(writer, entityList);
+  }
+
+}
