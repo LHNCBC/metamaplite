d8297030b39a8dc9ce8c18bc05df882dd62fcc3b
Willie Rogers <wjrogers@nih.gov>
2017-05-02 14:08:31 -0400

    Example of looking up terms while skipping the recognition step.

---
+++ src/main/java/examples/DirectLookup.java
@@ -0,0 +1,84 @@
+package examples;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Properties;
+import gov.nih.nlm.nls.metamap.lite.Normalization;
+import gov.nih.nlm.nls.metamap.lite.metamap.MetaMapIvfIndexes;
+import gov.nih.nlm.nls.ner.MetaMapLite;
+
+/**
+ * Example of looking up terms while skipping the recognition step.
+ *
+ *
+ * Created: Tue Apr 11 16:31:38 2017
+ *
+ * @author <a href="mailto:wjrogers@mail.nih.gov">Willie Rogers</a>
+ * @version 1.0
+ */
+public class DirectLookup {
+
+  Properties myProperties;
+  MetaMapIvfIndexes mmIndexes;
+  
+  /**
+   * Creates a new <code>DirectLookup</code> instance.
+   *
+   */
+  public DirectLookup()
+    throws IOException
+  {
+    this.myProperties = MetaMapLite.getDefaultConfiguration();
+    MetaMapLite.expandModelsDir(myProperties, "data/models");
+    MetaMapLite.expandIndexDir(myProperties,
+			       System.getProperty("metamaplite.index.directory",
+						  "data/ivf/2016AB/USAbase/strict"));
+    myProperties.setProperty("metamaplite.excluded.termsfile", "data/specialterms.txt");
+    this.mmIndexes = new MetaMapIvfIndexes(myProperties);
+  }
+
+  public List<String[]> lookup(String term)
+    throws IOException
+  {
+    List<String[]> hitList = new ArrayList<String[]>();
+    for (String doc: this.mmIndexes.cuiSourceInfoIndex.lookup(term, 3)) {
+      String[] fields = doc.split("\\|");
+      String cui = fields[0];
+      String docStr = fields[3];
+      hitList.add(fields);
+    }
+    return hitList;
+  }
+
+    /**
+   * Describe <code>main</code> method here.
+   *
+   * @param args a <code>String</code> value
+   */
+  public static final void main(final String[] args)
+    throws IOException
+  {
+
+    if (args.length > 0) {
+      StringBuilder sb = new StringBuilder();
+      for (String arg: args) {
+	sb.append(arg).append(" ");
+      }
+      DirectLookup instance = new DirectLookup();
+      int i = 0;
+      for (String term: sb.toString().split(",")) {
+	System.out.println("term:" + term);
+	for (String[] hit: instance.lookup(Normalization.normalizeLiteString(term))) {
+	  System.out.println(i + ": " + Arrays.toString(hit));
+	  i++;
+	}
+	System.out.println("i =" + i);
+      }
+    } else {
+      System.err.println("examples.DirectLookup multi-word-term");
+      System.err.println(" Terms are separated by commas.");
+    }
+  }
+}
