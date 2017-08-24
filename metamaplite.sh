325db4374992e444f9e2a9774284732ce2b65c70
Willie Rogers <wjrogers@nih.gov>
2017-08-24 14:16:46 -0400

    Updated ivf data path, added missing reference to treecode file
    UPdated data path to ivf/2017AA/USAbase.

--- metamaplite.sh
+++ metamaplite.sh
@@ -30,11 +30,12 @@ MMLPROPS="-Dopennlp.en-sent.bin.path=$OPENNLP_MODELS/en-sent.bin \
     -Dopennlp.en-pos.bin.path=$OPENNLP_MODELS/en-pos-perceptron.bin \
     -Dlog4j.configurationFile=$PROJECTDIR/config/log4j2.xml \
     -Dmetamaplite.entitylookup.resultlength=1500 \
-    -Dmetamaplite.ivf.cuiconceptindex=$PROJECTDIR/data/ivf/strict/indices/cuiconcept \
-    -Dmetamaplite.ivf.firstwordsofonewideindex=$PROJECTDIR/data/ivf/strict/indices/first_words_of_one_WIDE \
-    -Dmetamaplite.ivf.cuisourceinfoindex=$PROJECTDIR/data/ivf/strict/indices/cuisourceinfo \
-    -Dmetamaplite.ivf.cuisemantictypeindex=$PROJECTDIR/data/ivf/strict/indices/cuist \
-    -Dmetamaplite.ivf.varsindex=$PROJECTDIR/data/ivf/strict/indices/vars \
+    -Dmetamaplite.ivf.cuiconceptindex=$PROJECTDIR/data/ivf/2017AA/USAbase/strict/indices/cuiconcept \
+    -Dmetamaplite.ivf.firstwordsofonewideindex=$PROJECTDIR/data/ivf/2017AA/USAbase/strict/indices/first_words_of_one_WIDE \
+    -Dmetamaplite.ivf.cuisourceinfoindex=$PROJECTDIR/data/ivf/2017AA/USAbase/strict/indices/cuisourceinfo \
+    -Dmetamaplite.ivf.cuisemantictypeindex=$PROJECTDIR/data/ivf/2017AA/USAbase/strict/indices/cuist \
+    -Dmetamaplite.ivf.varsindex=$PROJECTDIR/data/ivf/2017AA/USAbase/strict/indices/vars \
+    -Dmetamaplite.ivf.meshtcrelaxedindex=$PROJECTDIR/data/ivf/2017AA/USAbase/strict/indices/meshtcrelaxed \
     -Dmetamaplite.excluded.termsfile=$PROJECTDIR/data/specialterms.txt"
 
 java $MML_JVM_OPTS -cp $PROJECTDIR/target/classes:$PROJECTDIR/build/classes:$PROJECTDIR/classes:$JARSPATH:$CONFIGDIR $MMLPROPS gov.nih.nlm.nls.ner.MetaMapLite $* 
