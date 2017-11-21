e0bd226a72c0a5e356cd33bb7f4bdb99e5f7b804
Willie Rogers <wjrogers@nih.gov>
2017-11-21 15:32:50 -0500

    Brat formatter now uses Brat standoff annotation library

--- src/main/java/gov/nih/nlm/nls/metamap/lite/resultformats/Brat.java
+++ src/main/java/gov/nih/nlm/nls/metamap/lite/resultformats/Brat.java
@@ -1,8 +1,6 @@
 //
 package gov.nih.nlm.nls.metamap.lite.resultformats;
 
-
-
 import bioc.BioCAnnotation;
 import bioc.BioCDocument;
 import bioc.BioCLocation;
@@ -12,6 +10,9 @@ import bioc.BioCSentence;
 import gov.nih.nlm.nls.metamap.lite.types.BioCEntity;
 import gov.nih.nlm.nls.metamap.lite.types.Entity;
 import gov.nih.nlm.nls.metamap.lite.types.Ev;
+
+import gov.nih.nlm.nls.tools.standoff.NormalizationAnnotation;
+import gov.nih.nlm.nls.tools.standoff.TextBoundAnnotation;
 
 import java.io.PrintWriter;
 import java.io.PrintStream;
@@ -27,10 +28,8 @@ import java.util.Set;
 import java.util.TreeMap;
 import java.util.Properties;
 
-import opennlp.tools.dictionary.serializer.Entry;
-
 /**
- *
+ * Implementation of Brat Standoff Format.
  */
 
 public class Brat implements ResultFormatter {
@@ -39,92 +38,13 @@ public class Brat implements ResultFormatter {
     System.getProperty("metamaplite.brat.typename", "MMLite");
   String textLabel = bratEntityTypeName;
   
-  static class TextBoundAnnotation {
-    String id;
-    String type;
-    int startOffset;
-    int endOffset;
-    String text;     
-    Set<NormalizationAnnotation> referenceSet = null;
-
-    public TextBoundAnnotation(String id, String type, int start, int end, String text) {
-      
-       this.id = id; 
-       this.type = type;
-       this.startOffset = start;
-       this.endOffset = end;
-       this.text = text.replaceAll("\n","\\n");
-    }
-    public TextBoundAnnotation(String id, String type, int start, int end, String text, 
-			       Set<NormalizationAnnotation> referenceSet) {
-       this.id = id; 
-       this.type = type;
-       this.startOffset = start;
-       this.endOffset = end;
-       this.text = text.replaceAll("\n","\\n");
-       this.referenceSet = referenceSet;
-    }
-    public void setId(String id) { this.id = id; }
-    public void setReferenceSet(Set<NormalizationAnnotation> referenceSet) { 
-      this.referenceSet = referenceSet;
-    }
-    public void addToReferenceSet(Set<NormalizationAnnotation> referenceSet) { 
-      this.referenceSet.addAll(referenceSet);
-    }
-    public Set<NormalizationAnnotation> getReferenceSet() { return this.referenceSet; } 
-    public String toString() {
-      return this.id + "\t" + this.type + " " + this.startOffset + " " + this.endOffset + "\t" + this.text;
-    }
-    public int hashCode() { return this.type.hashCode() + this.startOffset + this.endOffset + this.text.hashCode(); }
-    public String genKey() { return this.type.hashCode() + ":" + this.startOffset + ":" + this.endOffset + "|" + this.text.hashCode(); }
-  }
-  static class NormalizationAnnotation {
-    String id;
-    String target;
-    String rid;
-    String eid;
-    String text;
-    NormalizationAnnotation(String id, String target, String rid, String eid, String text) {
-      this.id = id.trim(); 
-      this.target = target.trim();
-      this.rid = rid.trim();
-      this.eid = eid.trim();
-      this.text = text.trim();
-    }
-    public boolean equals(Object obj) {
-      return  (this.rid.equals(((NormalizationAnnotation)obj).rid) &&
-	       this.eid.equals(((NormalizationAnnotation)obj).eid) &&
-	       this.text.equals(((NormalizationAnnotation)obj).text));
-    }
-    public int hashCode() {
-      return (this.rid + this.eid + this.text).hashCode();
-    }
-    public void setId(String id) { this.id = id; }
-    public void setTarget(String target) { this.target = target; }
-    public String getRid()  { return this.rid; }
-    public String getEid()  { return this.eid; }
-    public String getText() { return this.text; }
-    public String toString() {
-      return this.id + "\tReference " + this.target + " " + this.rid + ":" + this.eid + "\t" + this.text;
-    }
-  }
-
-  static class RelationAnnotation {
-    String id;
-    String type;
-    String arg1;
-    String arg2;
-    RelationAnnotation(String id, String type, String arg1, String arg2) {
-	this.id = id;
-	this.type = type;
-	this.arg1 = arg1;
-	this.arg2 = arg2;
-      }
-    public void setId(String id) { this.id = id; }
-    public void setArg1(String target) { this.arg1 = target; }
-    public void setArg2(String target) { this.arg2 = target; }
-  }
-
+  /**
+   * Generate reference set for entity as NormalizationAnnotation
+   * instances.
+   *
+   * @param entity Entity to convert to Normalization annotations
+   * @return set of normalization annotations
+   */
   public static Set<NormalizationAnnotation> generateReferenceSet(Entity entity) {
     Set<NormalizationAnnotation> referenceSet = new HashSet<NormalizationAnnotation>();
     if (entity.getScore() > 0.0) {
@@ -153,185 +73,14 @@ public class Brat implements ResultFormatter {
     return referenceSet;
   }
 
-  public static void writeAnnotationSet(String recognizerName,
-			   PrintWriter writer,
-			   Set<TextBoundAnnotation> annotationSet) {
-    int cindex = 0;
-    int nindex = 0;
-    for (TextBoundAnnotation annotation: annotationSet) {
-      cindex++;
-      String tid = "T" + cindex;
-      annotation.setId(tid);
-      writer.println(annotation.toString());
-      if (annotation.getReferenceSet() != null) {
-	for (NormalizationAnnotation nAnnotation: annotation.getReferenceSet()) {
-	  nindex++;
-	  nAnnotation.setId("N" + nindex);
-	  nAnnotation.setTarget(tid);
-	  writer.println(nAnnotation.toString());
-	}
-      }
-    }
-  }
-
-  public static void writeAnnotationSet(String recognizerName,
-			   PrintStream writer,
-			   Set<TextBoundAnnotation> annotationSet) {
-    int cindex = 0;
-    int nindex = 0;
-    for (TextBoundAnnotation annotation: annotationSet) {
-      cindex++;
-      String tid = "T" + cindex;
-      annotation.setId(tid);
-      writer.println(annotation.toString());
-      if (annotation.getReferenceSet() != null) {
-	for (NormalizationAnnotation nAnnotation: annotation.getReferenceSet()) {
-	  nindex++;
-	  nAnnotation.setId("N" + nindex);
-	  nAnnotation.setTarget(tid);
-	  writer.println(nAnnotation.toString());
-	}
-      }
-    }
-  }
-
-  public static void writeBratAnnotations(String recognizerName,
-					  PrintWriter writer,
-					  BioCDocument document) {
-    Map<BioCLocation,List<BioCAnnotation>> locationMap = new HashMap<BioCLocation,List<BioCAnnotation>>();
-    for (BioCPassage passage: document.getPassages()) {
-      for (BioCSentence sentence: passage.getSentences()) {
-	for (BioCAnnotation annotation: sentence.getAnnotations()) {
-	  for (BioCLocation location: annotation.getLocations()) {
-	    if (locationMap.containsKey(location)) {
-	      locationMap.get(location).add(annotation);
-	    } else {
-	      List<BioCAnnotation> annotationList = new ArrayList<BioCAnnotation>();
-	      annotationList.add(annotation);
-	      locationMap.put(location, annotationList);
-	    } /*if*/
-	  } /*for*/
-	} /*for*/
-      } /*for*/
-    } /*for*/
-
-    Set<TextBoundAnnotation> annotationSet = new HashSet<TextBoundAnnotation>();
-    for (Map.Entry<BioCLocation,List<BioCAnnotation>> entry: locationMap.entrySet()) {
-      BioCLocation location = entry.getKey();
-      for (BioCAnnotation annotation: entry.getValue()) {
-        int start = location.getOffset();
-	int end = start + location.getLength();
-	String term = annotation.getText();	  
-	if (annotation instanceof BioCEntity) {
-	  BioCEntity entityAnnotation = (BioCEntity)annotation;
-	  for (Entity entity: entityAnnotation.getEntitySet()) {
-	    annotationSet.add(new TextBoundAnnotation("T0",recognizerName,start,end,term,Brat.generateReferenceSet(entity)));
-	  }
-	} else {
-	  annotationSet.add(new TextBoundAnnotation("T0",recognizerName,start,end,term));
-	}
-      }
-    }
-    writeAnnotationSet(recognizerName, writer, annotationSet);
-  } /* writeBratAnnotations */
-
-
-  public static void listEntities(BioCSentence sentence) {
-    PrintStream writer = System.out;
-    String recognizerName = bratEntityTypeName;
-    Map<BioCLocation,List<BioCAnnotation>> locationMap = new HashMap<BioCLocation,List<BioCAnnotation>>();
-    for (BioCAnnotation annotation: sentence.getAnnotations()) {
-      for (BioCLocation location: annotation.getLocations()) {
-	if (locationMap.containsKey(location)) {
-	  locationMap.get(location).add(annotation);
-	} else {
-	  List<BioCAnnotation> annotationList = new ArrayList<BioCAnnotation>();
-	  annotationList.add(annotation);
-	  locationMap.put(location, annotationList);
-	} /*if*/
-      } /*for*/
-    } /*for*/
-
-    Map<String,TextBoundAnnotation> annotationMap = new HashMap<String,TextBoundAnnotation>();
-    for (Map.Entry<BioCLocation,List<BioCAnnotation>> entry: locationMap.entrySet()) {
-      BioCLocation location = entry.getKey();
-      for (BioCAnnotation annotation: entry.getValue()) {
-        int start = location.getOffset();
-	int end = location.getOffset() + location.getLength();
-	String term = annotation.getText();	  
-	TextBoundAnnotation textAnnot = new TextBoundAnnotation("T0",recognizerName,start,end,term);
-	if (annotation instanceof BioCEntity) {
-	  BioCEntity entityAnnotation = (BioCEntity)annotation;
-	  for (Entity entity: entityAnnotation.getEntitySet()) {
-	    if (annotationMap.containsKey(textAnnot.genKey())) {
-	      // textAnnot = annotationMap.get(textAnnot.genKey());
-	      // textAnnot.addToReferenceList(Brat.generateReferenceList(entity));
-	    } else {
-	      textAnnot.setReferenceSet(Brat.generateReferenceSet(entity));
-	      annotationMap.put(textAnnot.genKey(), textAnnot);
-	    }
-	  }
-	} else {
-	  annotationMap.put(textAnnot.genKey(), textAnnot);
-	}
-      }
-    }
-    Set<TextBoundAnnotation> annotationSet = new HashSet<TextBoundAnnotation>(annotationMap.values());
-    writeAnnotationSet(recognizerName, writer, annotationSet);
-  } /* listEntities */
-
-  public static void writeAnnotationList(String recognizerName,
-					 PrintWriter writer,
-					 List<Entity> entityList) {
-    Map<String,List<Entity>> locationMap = new HashMap<String,List<Entity>>();
-    for (Entity entity: entityList) {
-      String location = entity.getStart() + ":" + entity.getLength();
-      if (locationMap.containsKey(location)) {
-	locationMap.get(location).add(entity);
-      } else {
-	List<Entity> annotationList = new ArrayList<Entity>();
-	annotationList.add(entity);
-	locationMap.put(location, annotationList);
-      } /*if*/
-    } /*for*/
-    Map<String,TextBoundAnnotation> annotationMap = new HashMap<String,TextBoundAnnotation>();
-    for (Map.Entry<String,List<Entity>> entry: locationMap.entrySet()) {
-      for (Entity entity: entry.getValue()) {
-        int start = entity.getStart();
-	int end = start + entity.getLength();
-	String term = entity.getText();	  
-	TextBoundAnnotation textAnnot = new TextBoundAnnotation("T0",recognizerName,start,end,term);
-	if (annotationMap.containsKey(textAnnot.genKey())) {
-	  // textAnnot = annotationMap.get(textAnnot.genKey());
-	  // textAnnot.addToReferenceList(Brat.generateReferenceList(entity));
-	} else {
-	  textAnnot.setReferenceSet(Brat.generateReferenceSet(entity));
-	  annotationMap.put(textAnnot.genKey(), textAnnot);
-	}
-      }
-    }
-    Set<TextBoundAnnotation> annotationSet = new HashSet<TextBoundAnnotation>(annotationMap.values());
-    writeAnnotationSet(recognizerName, writer, annotationSet);
-  } /* listEntities */
-
-  public void setTextLabel(String newLabel) {
-    this.textLabel = newLabel;
-  }
-
-  public void entityListFormatter(PrintWriter writer,
-			   List<Entity> entityList) {
-    writeAnnotationList(this.textLabel, writer, entityList);
-  }
-
-  public void entityListFormatter(PrintWriter writer,
-				  List<Entity> entityList,
-				  String annotationTypeName) {
-    writeAnnotationList(annotationTypeName, writer, entityList);
-  }
-
-  
-  public static String annotationSetToString(String recognizerName,
-					   Set<TextBoundAnnotation> annotationSet) {
+  /**
+   * Write text bound annotations and associated reference annotations
+   * into string in Standoff annotation format.
+   * 
+   * @param annotationSet set of entity annotations with references.
+   * @return string of rendered entity and reference annotations.
+   */
+  public static String annotationSetToString(Set<TextBoundAnnotation> annotationSet) {
     StringBuilder sb = new StringBuilder();
     int cindex = 0;
     int nindex = 0;
@@ -350,10 +99,17 @@ public class Brat implements ResultFormatter {
       }
     }
     return sb.toString();
-  }
+  } /* annotationSetToString */
 
- public static String annotationListToString(String recognizerName,
-					     List<Entity> entityList) {
+  /**
+   * Write entities to string in Standoff annotation format.
+   * 
+   * @param recognizerName name to use for entity annotation typename 
+   * @param entityList list of entities to be converted to annotations
+   * @return string of rendered entity and reference annotations.
+   */
+  public static String annotationListToString(String recognizerName,
+					      List<Entity> entityList) {
     Map<String,List<Entity>> locationMap = new HashMap<String,List<Entity>>();
     for (Entity entity: entityList) {
       String location = entity.getStart() + ":" + entity.getLength();
@@ -382,23 +138,21 @@ public class Brat implements ResultFormatter {
       }
     }
     Set<TextBoundAnnotation> annotationSet = new HashSet<TextBoundAnnotation>(annotationMap.values());
-    return annotationSetToString(recognizerName, annotationSet);
-  } /* listEntities */
-  
+    return annotationSetToString(annotationSet);
+  } /* annotationListToString */
+
+  public void entityListFormatter(PrintWriter writer,
+				  List<Entity> entityList) {
+    writer.println(annotationListToString(this.textLabel, entityList));
+  }
 
   public String entityListFormatToString(List<Entity> entityList) {
     return annotationListToString(this.textLabel, entityList);
   }
 
-  public String entityListFormatToString(List<Entity> entityList,
-					 String annotationTypeName) {
-    return annotationListToString(annotationTypeName, entityList);
-  }
-
-
   public void initProperties(Properties properties) {
-       if (properties.containsKey("metamaplite.brat.typename")) {
-      bratEntityTypeName = properties.getProperty("metamaplite.brat.typename");
+    if (properties.containsKey("metamaplite.brat.typename")) {
+      this.textLabel = properties.getProperty("metamaplite.brat.typename");
     }
   }
 }
