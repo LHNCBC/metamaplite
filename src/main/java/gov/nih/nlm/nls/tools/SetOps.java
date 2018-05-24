824f6bfd1270f3845819a0c2e68a1aac1eee33a7
Willie Rogers <wjrogers@nih.gov>
2018-05-24 09:51:44 -0400

    Modified SetOps - Set Operations class
    Added symmetric_difference, added missing params to javadoc

--- src/main/java/gov/nih/nlm/nls/tools/SetOps.java
+++ src/main/java/gov/nih/nlm/nls/tools/SetOps.java
@@ -15,6 +15,7 @@ public class SetOps {
    * Return is set1 a subset set2
    * @param set1 a set of type T
    * @param set2 a set of type T
+   * @param <T> element type of set
    * @return true if set1 subset of set2
    */
   public static <T> boolean isSubsetOf(Set<T> set1,Set<T> set2) {
@@ -25,6 +26,7 @@ public class SetOps {
    * Return intersection of two sets
    * @param set1 a set of type T
    * @param set2 a set of type T
+   * @param <T> element type of set
    * @return set intersection of the two supplied sets
    */
   public static <T> Set<T> intersection(Set<T> set1,Set<T> set2) {
@@ -37,6 +39,7 @@ public class SetOps {
    * Return union of two sets
    * @param set1 a set of type T
    * @param set2 a set of type T
+   * @param <T> element type of set
    * @return set union of the two supplied sets
    */
   public static <T> Set<T> union(Set<T> set1,Set<T> set2) {
@@ -49,11 +52,29 @@ public class SetOps {
    * Return difference of two sets
    * @param set1 a set of type T
    * @param set2 a set of type T
+   * @param <T> element type of set
    * @return set difference of the two supplied sets
    */
   public static <T> Set<T> difference(Set<T> set1,Set<T> set2) {
     Set<T> differenceSet = new HashSet<T>(set1);
     differenceSet.removeAll(set2);
+    return differenceSet;
+  }
+
+  /**
+   * Return symmetric difference of two sets, essentially the
+   * disjunctive union.  That is, the union of the sets minus the
+   * intersection of the sets.
+   *
+   * @param set1 a set of type T
+   * @param set2 a set of type T
+   * @param <T> element type of set
+   * @return symmetric set difference of the two supplied sets
+   */
+
+  public static <T> Set<T> symmetric_difference(Set<T> set1,Set<T> set2) {
+    Set<T> differenceSet = union(set1,set2);
+    differenceSet.removeAll(intersection(set1,set2));
     return differenceSet;
   }
 }
