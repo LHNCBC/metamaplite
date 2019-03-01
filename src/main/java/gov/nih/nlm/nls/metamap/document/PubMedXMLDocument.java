04b6940f30f2d53444d9c2837b70a07ad70eadcd
Willie Rogers <wjrogers@nih.gov>
2019-03-01 14:43:33 -0500

    Replaced Pubmed XML reader implementation
    Re-implemented Pubmed XML reader to use StAX instead of DOM.

--- src/main/java/gov/nih/nlm/nls/metamap/document/PubMedXMLDocument.java
+++ src/main/java/gov/nih/nlm/nls/metamap/document/PubMedXMLDocument.java
@@ -5,82 +5,160 @@ package gov.nih.nlm.nls.metamap.document;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.ArrayList;
 import java.util.Map;
+
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.FileReader;
 import java.io.Reader;
-import gov.nih.nlm.nls.tools.Citation;
-import gov.nih.nlm.nls.tools.MedlineDomReader;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamReader;
+import javax.xml.stream.XMLStreamException;
 
 import bioc.BioCDocument;
 import bioc.BioCPassage;
 
 /**
+ * Convert a PubMed XML Document into a BioC document.
  *
+ * Created: Thu Aug 31 14:26:24 2017
+ *
+ * @author <a href="mailto:wjrogers@mail.nih.gov">Willie Rogers</a>
+ * @version 1.0
  */
 
 public class PubMedXMLDocument 
   implements BioCDocumentLoader
 {
-  MedlineDomReader medlineDomReader = new MedlineDomReader();
+  XMLInputFactory inputFactory = XMLInputFactory.newInstance();
 
   /**
-   * Instantiate PubMed XML as a BioC document.
-   * <p>
-
-   * @param citation citation instance
-   * @return BioC document instance
+   * <code>loadFileAsBioCDocument</code>- load PubXML document from
+   * filename as a BioCDocument
+   *
+   * @param filename a <code>Filename</code> value
+   * @return a <code>BioCDocument</code> value
+   * @exception FileNotFoundException if an error occurs
+   * @exception IOException if an error occurs
    */
-  public static BioCDocument instantiateBioCDocument(Citation citation) 
-  {
-    BioCDocument doc = new BioCDocument();
-    String pmid = citation.getSection("pmid");
-    if (pmid == null) {
-      pmid = "missing";
-    }
-    doc.setID(pmid);
-    BioCPassage titlePassage = new BioCPassage();
-    String titleText = citation.getSection("articleTitle");
-    titlePassage.setText(titleText);
-    titlePassage.setOffset(0);
-    titlePassage.putInfon("docid",pmid);
-    titlePassage.putInfon("section","title");
-    doc.addPassage(titlePassage);
-
-    BioCPassage abstractPassage = new BioCPassage();
-    abstractPassage.setText(citation.getSection("articleAbstract"));
-    abstractPassage.putInfon("docid",pmid);
-    abstractPassage.putInfon("section","abstract");
-    abstractPassage.setOffset(titleText.length());
-    doc.addPassage(abstractPassage);
-    return doc;
-  }
-
   @Override
   public BioCDocument loadFileAsBioCDocument(String filename)
     throws FileNotFoundException, IOException
   {
-    // TODO: Stub
-    return null;
+    BioCDocument doc = new BioCDocument();
+    try {
+      InputStream in = new FileInputStream(filename);
+      XMLStreamReader streamReader = this.inputFactory.createXMLStreamReader(in);
+      while (streamReader.hasNext()) {
+	if (streamReader.isStartElement()) {
+	  switch (streamReader.getLocalName())
+	    {
+	    case "PMID": {
+	      doc.setID(streamReader.getElementText());
+	      break;
+	    }
+	    case "ArticleTitle": {
+	      BioCPassage title = new BioCPassage();
+	      title.setText(streamReader.getElementText());
+	      title.setOffset(0);
+	      title.putInfon("docid", doc.getID());
+	      title.putInfon("section","title");
+	      doc.addPassage(title);
+		break;
+	    }
+	    case "AbstractText" : {
+	      System.out.print("AbstractText : ");
+	      System.out.println(streamReader.getElementText());
+	      System.out.print("Abstract : ");
+	      System.out.println(streamReader.getElementText());
+	      BioCPassage abstractPassage = new BioCPassage();
+	      abstractPassage.setText(streamReader.getElementText());
+	      abstractPassage.putInfon("docid", doc.getID());
+	      abstractPassage.putInfon("section","abstract");
+	      abstractPassage.setOffset(0);
+	      doc.addPassage(abstractPassage);
+	      break;
+	    }
+	    }
+	}
+	streamReader.next();
+      }
+    } catch (XMLStreamException xse) {
+      throw new RuntimeException(xse);
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+    return doc;
   }
 
   @Override
-  public List<BioCDocument> loadFileAsBioCDocumentList(String filename)
-    throws FileNotFoundException, IOException
+  public List<BioCDocument> readAsBioCDocumentList(Reader reader)
+    throws IOException
   {
     List<BioCDocument> documentList = new ArrayList<BioCDocument>();
-    Map<String,Citation> citationMap = this.medlineDomReader.readCitations(filename);
-    for (Citation citation: citationMap.values()) {
-      documentList.add(instantiateBioCDocument(citation));
+    try {
+      BioCDocument doc = new BioCDocument();
+      XMLStreamReader streamReader = this.inputFactory.createXMLStreamReader(reader);
+      while (streamReader.hasNext()) {
+	if (streamReader.isStartElement()) {
+	  switch (streamReader.getLocalName()) {
+	  case "PubmedArticle": {
+	    doc = new BioCDocument();
+	    documentList.add(doc);
+	    break;
+	  }
+	  case "PMID": {
+	    doc.setID(streamReader.getElementText());
+	    break;
+	  }
+	  case "ArticleTitle": {
+	    BioCPassage title = new BioCPassage();
+	    title.setText(streamReader.getElementText());
+	    title.setOffset(0);
+	    title.putInfon("docid", doc.getID());
+	    title.putInfon("section","title");
+	    doc.addPassage(title);
+	    break;
+	  }
+	  case "AbstractText" : {
+	    BioCPassage abstractPassage = new BioCPassage();
+	    abstractPassage.setText(streamReader.getElementText());
+	    abstractPassage.putInfon("docid", doc.getID());
+	    abstractPassage.putInfon("section","abstract");
+	    abstractPassage.setOffset(0);
+	    doc.addPassage(abstractPassage);
+	    break;
+	  }
+	  }
+	}
+	streamReader.next();
+      }      
+    } catch (XMLStreamException xse) {
+      throw new RuntimeException(xse);
+    } catch (Exception e) {
+      throw new RuntimeException(e);
     }
     return documentList;
   }
 
+  /**
+   * <code>readAsBioCDocumentList</code> - load a set of PubmedXML
+   * documents from filename.
+   *
+   * @param filename a <code>Filename</code> value
+   * @return a <code>List</code> value
+   * @exception FileNotFoundException if an error occurs
+   * @exception IOException if an error occurs
+   */
   @Override
-  public List<BioCDocument> readAsBioCDocumentList(Reader Reader)
-    throws IOException
+  public List<BioCDocument> loadFileAsBioCDocumentList(String filename)
+    throws FileNotFoundException, IOException
   {
-    // TODO: Stub
-    return null;
+    return readAsBioCDocumentList(new FileReader(filename));
   }
+
 }
