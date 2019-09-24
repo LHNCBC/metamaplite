83dee11525e7a0045b5a4730b0a9a38d6cd69ca4
Willie Rogers <wjrogers@nih.gov>
2019-09-24 16:52:34 -0400

    Refactored IVFLookup to conform to DictionaryLookup interface.

--- src/main/java/gov/nih/nlm/nls/metamap/lite/IVFLookup.java
+++ src/main/java/gov/nih/nlm/nls/metamap/lite/IVFLookup.java
@@ -2,6 +2,7 @@ package gov.nih.nlm.nls.metamap.lite;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.File;
 
 import java.util.List;
 import java.util.Set;
@@ -14,6 +15,8 @@ import org.apache.logging.log4j.Logger;
 import gov.nih.nlm.nls.metamap.prefix.Token;
 import gov.nih.nlm.nls.metamap.prefix.ERToken;
 import gov.nih.nlm.nls.metamap.lite.types.ConceptInfo;
+import gov.nih.nlm.nls.metamap.lite.dictionary.MMLDictionaryLookup;
+import gov.nih.nlm.nls.metamap.lite.dictionary.MMLDictionaryLookupRegistry;
 
 /**
  * Describe class IVFLookup here.
@@ -24,7 +27,7 @@ import gov.nih.nlm.nls.metamap.lite.types.ConceptInfo;
  * @author <a href="mailto:wjrogers@mail.nih.gov">Willie Rogers</a>
  * @version 1.0
  */
-public class IVFLookup implements DictionaryLookup<TermInfo> {
+public class IVFLookup implements MMLDictionaryLookup<TermInfo> {
   private static final Logger logger = LogManager.getLogger(IVFLookup.class);
 
   /** string column for cuisourceinfo index*/
@@ -48,49 +51,38 @@ public class IVFLookup implements DictionaryLookup<TermInfo> {
   /** word to variant lookup */
   VariantLookupIVF variantLookup;
 
-  public static class IVFTermInfo implements TermInfo {
-    String originalTerm;
-    String normTerm;
-    Set<ConceptInfo> dictionaryInfo;
-    List<? extends Token> tokenList;
-    public IVFTermInfo(String originalTerm, 
-		       String normTerm,
-		       Set<ConceptInfo> dictionaryInfo,
-		       List<? extends Token> tokenSubList) {
-      this.originalTerm = originalTerm;
-      this.normTerm = normTerm;
-      this.dictionaryInfo = dictionaryInfo;
-      this.tokenList = tokenSubList;
-    }
-    public IVFTermInfo(String originalTerm, 
-		       String normTerm,
-		       Set<ConceptInfo> dictionaryInfo) {
-      this.originalTerm = originalTerm;
-      this.normTerm = normTerm;
-      this.dictionaryInfo = dictionaryInfo;
-    }
-    /** normalized form of term */
-    public String getNormTerm() { return this.normTerm; }
-    /** original term */
-    public String getOriginalTerm() { return this.originalTerm; }
-    /** dictionary info */
-    public Set<ConceptInfo> getDictionaryInfo() { return this.dictionaryInfo; }
-    public List<? extends Token> getTokenList() { return this.tokenList; }
-    public String toString() { return this.originalTerm + "|" + this.normTerm + "|" + this.dictionaryInfo; }
+
+  /**
+   * Creates a new <code>IVFLookup</code> instance.
+   *
+   */
+  public IVFLookup() {
   }
 
   /**
    * Creates a new <code>IVFLookup</code> instance.
-   * @param properties metamaplite configuration properties instance
+   * @param properties application properties
    */
   public IVFLookup(Properties properties) {
+    init(properties);
+  }
+
+  // Implementation of gov.nih.nlm.nls.metamap.lite.DictionaryLookup
+
+
+  public void init(Properties properties) {
     try {
+      if (properties.containsKey("metamaplite.excluded.termsfile")) {
+	this.excludedTerms.addTerms(properties.getProperty("metamaplite.excluded.termsfile"));
+      } else if (System.getProperty("metamaplite.excluded.termsfile") != null) {
+	this.excludedTerms.addTerms(System.getProperty("metamaplite.excluded.termsfile"));
+      }
       this.MAX_TOKEN_SIZE =
 	Integer.parseInt(properties.getProperty("metamaplite.entitylookup3.maxtokensize",
 						Integer.toString(MAX_TOKEN_SIZE)));
 
       this.mmIndexes = new MetaMapIvfIndexes(properties);
-      this.cuiPreferredNameCache = new CuiPreferredNameCache(properties, mmIndexes);
+      this.cuiPreferredNameCache = new CuiPreferredNameCache(this, properties);
       this.cuiSemanticTypeSetIndex = new CuiSemanticTypeSetIndex(mmIndexes);
       this.cuiSourceSetIndex = new CuiSourceSetIndex(mmIndexes);
       this.termConceptInfoCache = new TermConceptInfoCache(properties,
@@ -105,27 +97,72 @@ public class IVFLookup implements DictionaryLookup<TermInfo> {
     }
   }
 
-  // Implementation of gov.nih.nlm.nls.metamap.lite.DictionaryLookup
+  public String getPreferredName(String cui) {
+    try {
+      List<String> hitList = 
+	this.mmIndexes.cuiConceptIndex.lookup(cui, 0);
+      if (hitList.size() > 0) {
+	String[] fields = hitList.get(0).split("\\|");
+	return fields[1];
+      }
+      return null;
+    } catch (IOException ioe) {
+      throw new RuntimeException(ioe);
+    }
+  }
+
+  public Set<String> getSourceSet(String cui) {
+    try {
+      return this.cuiSourceSetIndex.getSourceSet(cui);
+    } catch (FileNotFoundException fnfe) {
+      throw new RuntimeException(fnfe);
+    } catch (IOException ioe) {
+      throw new RuntimeException(ioe);
+    }
+  }
+  
+  public Set<String> getSemanticTypeSet(String cui) {
+    try {
+      return this.cuiSemanticTypeSetIndex.getSemanticTypeSet(cui);
+    } catch (FileNotFoundException fnfe) {
+      throw new RuntimeException(fnfe);
+    } catch (IOException ioe) {
+      throw new RuntimeException(ioe);
+    }
+  }
 
   /**
    * Lookup term in IVF index 
    *
-   * @param originalTerm original term
-   * @param normTerm normalized form of term
-   * @param tokenSubList a tokenlist 
-   * @return a <code>TermInfo</code> value associated with input term.
+   * @param term term
+   * @return a <code>String</code> array associated with input term.
    */
-  public final TermInfo lookup(final String originalTerm, 
-			       final String normTerm, 
-			       final List<? extends Token> tokenSubList) {
+  public final TermInfo lookup(final String term) {
+    Set<ConceptInfo> conceptInfoSet = new HashSet<ConceptInfo>();
+    String originalTerm = term;
+    String normTerm = term;
     try {
-    return new IVFTermInfo
-      (originalTerm,
-       normTerm,
-       this.termConceptInfoCache.lookupTermConceptInfo(originalTerm,
-						       normTerm,
-						       tokenSubList),
-       tokenSubList);
+      for (String doc: this.mmIndexes.cuiSourceInfoIndex.lookup(term, 3)) {
+	String[] fields = doc.split("\\|");
+	String cui = fields[0];
+	String docStr = fields[3];
+
+	// Note: check for (! excludedTerms.isExcluded(cui,normTerm)) &&
+	// (isLikelyMatch(originalTerm,normTerm,docStr))
+	// should done one level up
+	
+	// If term is not in excluded term list and term or
+	// normalized form of term matches lookup string or
+	// normalized form of lookup string then get
+	// information about lookup string.
+	ConceptInfo conceptInfo = new ConceptInfo(cui,
+						  this.cuiPreferredNameCache.findPreferredName(cui),
+						  docStr,
+						  this.cuiSourceSetIndex.getSourceSet(cui),
+						  this.cuiSemanticTypeSetIndex.getSemanticTypeSet(cui));
+	conceptInfoSet.add(conceptInfo);
+      }
+      return new TermInfoImpl<Set<ConceptInfo>>(originalTerm, normTerm, conceptInfoSet);
     } catch (FileNotFoundException fnfe) {
       throw new RuntimeException(fnfe);
     } catch (IOException ioe) {
@@ -133,29 +170,12 @@ public class IVFLookup implements DictionaryLookup<TermInfo> {
     }
   }
 
-
-  /**
-   * Lookup term in IVF index
-   *
-   * @param originalTerm original term
-   * @param normTerm normalized form of term
-   * @return a <code>TermInfo</code> value associated with input term.
-   */
-  public final TermInfo lookup(final String originalTerm, 
-			       final String normTerm) {
-    try {
-    return new IVFTermInfo
-      (originalTerm,
-       normTerm,
-       this.termConceptInfoCache.lookupTermConceptInfo(originalTerm,
-						       normTerm));
-    } catch (FileNotFoundException fnfe) {
-      throw new RuntimeException(fnfe);
-    } catch (IOException ioe) {
-      throw new RuntimeException(ioe);
+  public boolean verifyImplementation(String directoryPath) {
+    if ((new File(directoryPath + "/indices").exists()) &&
+	(new File(directoryPath + "/tables").exists())) {
+      return true;
+    } else {
+      return false;
     }
   }
-
-  
 }
-  
