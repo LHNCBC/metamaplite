93aa068f611f976c0bd9564ec5a929032c837125
Willie Rogers <wjrogers@nih.gov>
2020-04-03 13:42:39 -0400

    Updated windows version of frontend script

--- metamaplite.bat
+++ metamaplite.bat
@@ -24,12 +24,12 @@ set JVMOPTS=-Den-sent.bin.path=%OPENNLP_MODELS%/en-sent.bin ^
 -Dlog4j.configurationFile=file:///%projectdir%/config/log4j2.xml ^
 -Dmetamaplite.property.file=%CONFIGDIR%/metamaplite.properties ^
 -Dmetamaplite.entitylookup.resultlength=1500 ^
--Dmetamaplite.ivf.cuiconceptindex=%projectdir%/data/ivf/2018AA/USAbase/strict/cuiconcept ^
--Dmetamaplite.ivf.firstwordsofonewideindex=%projectdir%/data/ivf/2018AA/USAbase/strict/first_words_of_one_WIDE ^
--Dmetamaplite.ivf.cuisourceinfoindex=%projectdir%/data/ivf/2018AA/USAbase/strict/cuisourceinfo ^
--Dmetamaplite.ivf.cuisemantictypeindex=%projectdir%/data/ivf/2018AA/USAbase/strict/cuist ^
--Dmetamaplite.ivf.varsindex=%projectdir%/data/ivf/2018AA/USAbase/strict/vars ^
--Dmetamaplite.ivf.meshtcrelaxedindex=$PROJECTDIR/data/ivf/2018AA/USAbase/strict/indices/meshtcrelaxed
+-Dmetamaplite.ivf.cuiconceptindex=%projectdir%/data/ivf/strict/cuiconcept ^
+-Dmetamaplite.ivf.firstwordsofonewideindex=%projectdir%/data/ivf/strict/first_words_of_one_WIDE ^
+-Dmetamaplite.ivf.cuisourceinfoindex=%projectdir%/data/ivf/strict/cuisourceinfo ^
+-Dmetamaplite.ivf.cuisemantictypeindex=%projectdir%/data/ivf/strict/cuist ^
+-Dmetamaplite.ivf.varsindex=%projectdir%/data/ivf/strict/vars ^
+-Dmetamaplite.ivf.meshtcrelaxedindex=$PROJECTDIR/data/ivf/strict/indices/meshtcrelaxed
 
 java -cp %projectdir%/target/classes;%projectdir%/build/classes;%projectdir%/classes;%JARSPATH%;%CONFIGDIR% ^
      %JVMOPTS% gov.nih.nlm.nls.ner.MetaMapLite %* 
