e7c06e95e9b762ba3a8da88caf62cd242669816e
Willie Rogers <wjrogers@nih.gov>
2020-08-12 16:54:31 -0400

    Removed string intern calls from set methods in ConceptInfo.

--- src/main/java/gov/nih/nlm/nls/metamap/lite/types/ConceptInfo.java
+++ src/main/java/gov/nih/nlm/nls/metamap/lite/types/ConceptInfo.java
@@ -31,7 +31,7 @@ public class ConceptInfo {
 		     Set<String> newSemanticTypeSet) {
     // this.cui = cui.intern();
     this.cui = cui;
-    this.preferredName = prefname.intern();
+    this.preferredName = prefname;
     this.conceptString = conceptString;
     this.sourceSet = newSourceSet;
     this.semanticTypeSet = newSemanticTypeSet;
@@ -41,7 +41,7 @@ public class ConceptInfo {
 		     Set<String> newSemanticTypeSet) {
     // this.cui = cui.intern();
     this.cui = cui;
-    this.preferredName = prefname.intern();
+    this.preferredName = prefname;
     this.sourceSet = newSourceSet;
     this.semanticTypeSet = newSemanticTypeSet;
   }
@@ -55,11 +55,11 @@ public class ConceptInfo {
   }
 
   public String getCUI() { return this.cui; }
-  public void setCUI(String cui) { this.cui = cui.intern(); }
+  public void setCUI(String cui) { this.cui = cui; }
   public String getPreferredName() { return this.preferredName; }
-  public void setPreferredName(String name) { this.preferredName = name.intern(); }
+  public void setPreferredName(String name) { this.preferredName = name; }
   public String getConceptString() { return this.conceptString; }
-  public void setConceptString(String name) { this.conceptString = name.intern(); }
+  public void setConceptString(String name) { this.conceptString = name; }
   public void addSourceSet(Collection<String> newSourceList) {
     this.sourceSet.addAll(newSourceList);
   }
