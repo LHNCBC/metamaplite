2cb64edad24317a78ac362ab37f384a361961952
Rogers <wjrogers@mail.nih.gov>
2025-12-08 12:59:33 -0500

    Partial fixes to ScannerTest
    Updated to allow it to compile and pass tests.

--- src/test/java/gov/nih/nlm/nls/metamap/prefix/ScannerTest.java
+++ src/test/java/gov/nih/nlm/nls/metamap/prefix/ScannerTest.java
@@ -3,6 +3,7 @@
 package gov.nih.nlm.nls.metamap.prefix;
 
 import java.util.List;
+import java.util.ArrayList;
 
 import org.junit.Test;
 import org.junit.Ignore;
@@ -11,6 +12,8 @@ import org.junit.runners.JUnit4;
 import org.junit.Assert.*;
 
 import gov.nih.nlm.nls.types.Sentence;
+import gov.nih.nlm.nls.metamap.prefix.Scanner;
+
 import bioc.BioCSentence;
 
 
@@ -22,11 +25,19 @@ import bioc.BioCSentence;
  */
 @RunWith(JUnit4.class)
 public class ScannerTest {
+  String sentence = "Cervical cancer starts in the cells of the cervix, which is the lower, narrow end of the uterus.";
+  List<ERToken> tokenlist;
+  List<ClassifiedToken> classTokenlist;
+  List<Token> offsetTokenlist;
+
   @org.junit.Before public void setup() {
+    this.tokenlist = Scanner.analyzeText(sentence);
+    this.classTokenlist = new ArrayList<ClassifiedToken>();
+    this.offsetTokenlist = new ArrayList<Token>();
   }
 
   @org.junit.Test public void testAddOffsets() {
-    
+    // assertEquals(addOffsets(this.tokenlist, 0), )
   }
   
   @org.junit.Test public void testAnalyzeText() {
